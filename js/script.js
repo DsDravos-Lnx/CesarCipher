0c8fde09dd3be54fcc75c8f5a4c27bdbca87e8bc
DsDravos-lnx <ds.hallucinate@gmail.com>
2020-01-29 16:37:17 -0300

    caesarCipher

--- js/script.js
+++ js/script.js
@@ -1,34 +1,49 @@
-(function() {
-
-    const cipherOption = prompt('Deseja CRIPTOGRAFAR ou DESCRIPTOGRAFAR?')
-
-    const caesarCipher = function (option, key, cipher) {
+(function(){
+    const cipherOption = Number(prompt('Deseja CRIPTOGRAFAR(1) ou DESCRIPTOGRAFAR(2):'))
+    const key = Number(prompt('Digite o numero da chave:'))
+    const caesarCipher = function (option, key, cipher){
         const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        const cipherVector = cipher.split(' ')
-        const cipherLenght = Number(cipher.length - (cipherVector.length - 1))
-        
-        cipherVector = cipherVector.toUpperCase
+        let msg = ''
 
-        if(option == 1){
-            
-            for( let i=0; i<cipherLenght; i++){
-                
+        for(let i=0; i<cipher.length; i++){
+            for(let j=0; j<abc.length; j++){
+                if(cipher[i].toUpperCase() == abc[j]){
+                    if (option == 1){
+                        if((key+j)<26){
+                            msg += abc[key+j]
+                        } else {
+                            msg += abc[((key+j)-26)]
+                        }
+                    } else {
+                        if((j-key)>=0){
+                            msg += abc[j-key]
+                        } else {
+                            msg += abc[((j-key)+26)]
+                        }
+                    }                  
+                } else if (cipher[i] == ' ') {
+                    msg += ' '
+                    
+                } else {
+                    specialChar = /[A-Z]/
+                    if (specialChar.test(cipher[i].toUpperCase()) == false){
+                        msg += cipher[i]
+                    
+                    }
+                }
             }
-        } else [
-
-        ]
+        }
+        document.write(msg)
     }
 
-    if (cipherOption.toUpperCase == 'CRIPTOGRAFAR'){
-        let key = Number(prompt('Digite o numero da chave:'))
-        let cipher = prompt('Digite a mensagem:')
-        caesarCipher(1, 2, 'alex alves')
-    } else if (cipherOption.toUpperCase == 'DESCRIPTOGRAFAR'){
-        let key = Number(prompt('digite o numero da key:'))
-        let cipher = prompt('Digite a cifra:')
-
+    if(cipherOption == 1 && key < 26){
+        let cipher = prompt('Digite sua mensagem:')
+        caesarCipher(1, key, cipher)
+    } else if(cipherOption == 2 && key < 26){
+        let cipher = prompt('Digite sua cifra:')
+        caesarCipher(2, key, cipher)
     } else {
-        alert('A opçao desejada nao exite, digite CRIPTOGRAFAR ou DESCRIPTOGRAFAR')
+        alert('A opcao digitada ou a chave nao e valida')
     }
 
 })()
